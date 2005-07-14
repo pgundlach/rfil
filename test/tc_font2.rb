aebac1e3aad2e58f0db48481647631b40c72307f
Patrick Gundlach <gundlach@speedata.de>
2005-07-14 01:46:24 +0000

    new test, split off from tc_font.rb

---
+++ test/tc_font2.rb
@@ -0,0 +1,41 @@
+#!/opt/ruby/1.8/bin/ruby
+
+require 'test/unit'
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'font'
+require 'enc'
+require 'pl'
+require 'plparser'
+require 'kpathsea'
+require 'pp'
+
+class TestFontb < Test::Unit::TestCase
+  def test_write
+    t=Time.new
+    # I want to see which files actually changed. A more clever solution?
+    sleep 1
+    
+    destdir=File.join(Dir.getwd,"output")
+    f=File.join(destdir,"savorg__.vf")
+    if File.exists? f
+      assert(File.mtime(f) < t)
+    end
+    font=Font.new
+    font.load_variant("savorg__.afm")
+    font.set_dirs(destdir)
+    font.write_files
+    assert(File.mtime(File.join(destdir,"savorg__.vf")) > t)
+    font.texenc=["ec.enc","texnansi.enc"]
+    #font.texenc=["texnansi.enc"]
+    font.write_files
+    
+    filepath=(File.join(destdir,"ec-savorg__.vf"))
+    assert(File.exists?(filepath), "I cannot find: #{filepath}")
+    assert(File.mtime(filepath) > t,
+           "The file #{filepath} did not change as I thought it would.")
+    assert(File.mtime(File.join(destdir,"texnansi-savorg__.vf")) > t)
+  end
+  
+end
