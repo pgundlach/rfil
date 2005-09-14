d7ee0f0d04948c8aa6eac340375b3c91200edb0e
Patrick Gundlach <gundlach@speedata.de>
2005-09-14 13:24:13 +0000

    adapted to rfii, won't run with old installer

--- lib/rfi_plugin_latex.rb
+++ lib/rfi_plugin_latex.rb
@@ -8,7 +8,7 @@ class FDWriterLaTeX < RFI::Plugin
   
   def initialize(fontcollection)
     @fc=fontcollection
-    super(:latex)
+    super(:latex,:sty)
   end
 
   def run_plugin
@@ -26,7 +26,7 @@ class FDWriterLaTeX < RFI::Plugin
   # example, should be an extra plugin
   def latex_fd(e)
     latexenc=case e.encname
-             when "ECEncoding"
+             when "ECEncoding","T1Encoding"
                "T1"
              when "TeXBase1Encoding"
                "8r"
@@ -35,39 +35,56 @@ class FDWriterLaTeX < RFI::Plugin
              else
                raise "unknown latex encoding: #{e.encname}"
              end
-    filename="#{latexenc}#{@fc.fontname}.fd"
+    filename="#{latexenc}#{@fc.name}.fd"
 
     fd="\\ProvidesFile{#{filename}}
-\\DeclareFontFamily{#{latexenc}}{#{@fc.fontname}}{}
+\\DeclareFontFamily{#{latexenc}}{#{@fc.name}}{}
 "
-    [[:m,:n],[:m,:sc],[:m,:sl],[:m,:it],
-      [:b,:n],[:b,:sc],[:b,:sl],[:b,:it],
-      [:bx,:n],[:bx,:sc],[:bx,:sl],[:bx,:it]].each{ |w,v|
+    weight=[:m,:b,:bx]
+    variant=[:n,:sc,:sl,:it]
+    for i in 0..11
+      w=weight[i/4]
+      v=variant[i % 4]
       f=find_font(w,v)
-      name = f ? f.tex_fontname(e) : "<->ssub * #{@fc.fontname}/n/n"
-      fd << "\\DeclareFontShape{#{latexenc}}{#{@fc.fontname}}{#{w}}{#{v}}{
+      if f
+        name = f.tex_fontname(e)
+      else
+        if i < 4
+          name = "ssub * #{@fc.name}/m/n"
+        else
+          name = "ssub * #{@fc.name}/#{weight[i/4 - 1]}/#{v}"
+        end
+      end
+        
+#     [[:m,:n],[:m,:sc],[:m,:sl],[:m,:it],
+#       [:b,:n],[:b,:sc],[:b,:sl],[:b,:it],
+#       [:bx,:n],[:bx,:sc],[:bx,:sl],[:bx,:it]].each{ |w,v|
+#       f=find_font(w,v)
+      
+#       name = f ? f.tex_fontname(e) : "<->ssub * #{@fc.name}/m/n"
+      fd << "\\DeclareFontShape{#{latexenc}}{#{@fc.name}}{#{w}}{#{v}}{
       <->   #{name}
 }{}
 "
-    }
-    return [filename,fd]
-  end
+    end
+  return [filename,fd]
+end
   def find_font(w,v)
     weight={}
     variant={}
     weight[:m]=:regular
     weight[:b]=:bold
     variant[:n]=:regular
-    variant[:i]=:italic
+    variant[:it]=:italic
     variant[:sl]=:slanted
     variant[:sc]=:smallcaps
 
     # w is one of :m, :b, :bx
     # v is one of :n, :sc, :sl, :it
-    @fc.fonts.each { |a,b|
+    @fc.fonts.each { |font|
       #p b[:weight]==weight[w]
-      if b[:variant]==variant[v] and b[:weight]==weight[w]
-        return a
+      if font.variant ==variant[v] and font.weight==weight[w]
+        return font
       end
     }
     return nil
